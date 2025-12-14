431fddfd73fdbc4e5bbd16803e6937766a9fb380
Vartec <misticmvm@gmail.com>
2025-12-14 20:07:14 +0300

    feat(oauth): Adding a TokenOAuth model with access and refresh token fields

---
+++ lib/features/cloud_sync/oauth/models/token_oauth.dart
@@ -0,0 +1,32 @@
+// String get accessToken;
+//   String get refreshToken;
+//   String get userName;
+//   String get iss;
+//   bool get timeToRefresh;
+//   bool get canRefresh;
+//   bool get timeToLogin;
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:cloud_storage_all/cloud_storage_all.dart' show OAuth2Token;
+
+part 'token_oauth.freezed.dart';
+part 'token_oauth.g.dart';
+
+@freezed
+abstract class TokenOAuth with _$TokenOAuth {
+  const factory TokenOAuth({
+    required String id,
+    required String accessToken,
+    required String refreshToken,
+    required String userName,
+    required String iss,
+    required bool timeToRefresh,
+    required bool canRefresh,
+    required bool timeToLogin,
+    required String tokenJson,
+  }) = _TokenOAuth;
+
+  // fromJson
+  factory TokenOAuth.fromJson(Map<String, dynamic> json) =>
+      _$TokenOAuthFromJson(json);
+}
